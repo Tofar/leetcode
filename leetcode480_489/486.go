72122f64d6771fabd337d4d61d69e3c3595ffd42
mu-mo <yun_tofar@qq.com>
2018-10-19 19:52:48 +0800

    add leetcode 485 486

---
+++ leetcode480_489/486.go
@@ -0,0 +1,26 @@
+package leetcode480_489
+
+func PredictTheWinner(nums []int) bool {
+	return winNum(nums) >= 0
+}
+
+func winNum(nums []int) int {
+	switch len(nums) {
+	case 0:
+		return 0
+	case 1:
+		return nums[0]
+	case 2:
+		if nums[0] > nums[1] {
+			return nums[0] - nums[1]
+		}
+		return nums[1] - nums[0]
+	default:
+		num1 := nums[0] - winNum(nums[1:])
+		num2 := nums[len(nums)-1] - winNum(nums[:(len(nums)-1)])
+		if num1 > num2 {
+			return num1
+		}
+		return num2
+	}
+}
