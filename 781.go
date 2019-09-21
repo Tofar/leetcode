a8ee7ae835ef7428e62da0394932b42aefb2ab51
Ethan <yun_tofar@qq.com>
2019-09-21 17:27:22 +0800

    Update 781.go

--- 781.go
+++ 781.go
@@ -2,21 +2,13 @@ func numRabbits(answers []int) int {
     if len(answers) == 0 {
         return 0
     }
-    sort.Ints(answers)
+    answersMap := map[int]int{}
+    for _, v := range answers {
+        answersMap[v]++
+    }
     sum := 0
-    for i := 0; i < len(answers); i++ {
-        cur := answers[i]
-        sum += cur+1
-        j := i+cur
-        if i+cur >= len(answers) {
-            j = len(answers)-1
-        }
-        for ; j > i; j-- {
-            if answers[j] == cur {
-                break
-            }
-        }
-        i = j
+    for k, v := range answersMap {
+        sum += (k+1)*(int(math.Ceil(float64(v)/float64(k+1))))
     }
     
     return sum
